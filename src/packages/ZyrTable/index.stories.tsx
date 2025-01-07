47288103ddb67114b584078d8a6e0af6defde141
zhangyanru <zhangyanru@wshifu.com>
2025-01-07 18:24:31 +0800

    feat: zytTable

--- src/packages/ZyrTable/index.stories.tsx
+++ src/packages/ZyrTable/index.stories.tsx
@@ -1,6 +1,7 @@
 import type { Meta, StoryObj } from "@storybook/react";
 import React from "react";
 import ZyrTable from "./index";
+import { SettingOutlined } from "@ant-design/icons";
 
 const meta: Meta<typeof ZyrTable> = {
   title: "Show/ZyrTable",
@@ -10,6 +11,49 @@ const meta: Meta<typeof ZyrTable> = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-export const Default: Story = {
-  args: {},
+/**
+ * 基本使用
+ */
+export const DefaultTable: Story = {
+  args: {
+    column: {
+      name1: {
+        key: "name1",
+        title: (
+          <div>
+            第1列
+            <SettingOutlined style={{ color: "red" }} />
+          </div>
+        ),
+      },
+      name2: {
+        key: "name2",
+        title: "第2列",
+      },
+      name3: {
+        key: "name3",
+        title: "第3列",
+      },
+      name4: {
+        key: "name4",
+        title: "第4列",
+        render: (val, item, index) => {
+          return (
+            <div style={{ color: "red" }}>
+              <div>render字段值：{val}</div>
+              <div>name3的值：{item.name3}</div>
+              <div> 索引：{index}</div>
+            </div>
+          );
+        },
+      },
+    },
+    extraColumn: (val) => {
+      return <div style={{ color: "green" }}>name1字段值：{val.name1}</div>;
+    },
+    dataSource: [
+      { name1: "1", name2: "2", name3: "3", name4: "4" },
+      { name1: "11", name2: "22", name3: "33", name4: "44" },
+    ],
+  },
 };
--- src/packages/ZyrTable/index.tsx
+++ src/packages/ZyrTable/index.tsx
@@ -1,13 +1,71 @@
+import "./index.less";
+import React, { Fragment } from "react";
+import cn from "classnames";
+import { createNamespace } from "../../utils/createBEM";
 
-    import "./index.less";
-    import React from "react";
-    import cn from "classnames";
-    import { createNamespace } from "../../utils/createBEM";
+type ItemProps = {
+  key: string;
+  title: string | React.ReactNode;
+  headerStyle?: React.CSSProperties;
+  cellStyle?: React.CSSProperties;
+  render: (val: any, item?: Object, index?: number) => React.ReactNode;
+};
+export interface ZyrTableProps {
+  column: { [key: string]: ItemProps };
+  extraColumn: React.ReactNode | ((val: any) => React.ReactNode);
+  dataSource: object[];
+}
 
-    export interface ZyrTableProps {}
+const bem = createNamespace("table");
+const ZyrTable = ({ column, extraColumn, dataSource }: ZyrTableProps) => {
+  const getTableHeader = () => {
+    return Object.keys(column).map((key) => {
+      return (
+        <th key={key} style={{ ...column[key].headerStyle }}>
+          {column[key].title}
+        </th>
+      );
+    });
+  };
 
-    const bem = createNamespace("table");
-    const ZyrTable = ({}: ZyrTableProps) => { return <div></div>}
+  const getTableBodyRow = () => {
+    return dataSource.map((item, index) => {
+      return (
+        <Fragment key={index}>
+          <tr className={cn(bem.e("empty"))}></tr>
+          <tr className={cn(bem.e("extra"))}>
+            <td colSpan={Object.keys(column).length}>
+              {typeof extraColumn === "function"
+                ? extraColumn(item)
+                : extraColumn}
+            </td>
+          </tr>
+          <tr>
+            {Object.keys(column).map((key) => {
+              return (
+                <td key={key} style={{ ...column[key].cellStyle }}>
+                  {column[key].render
+                    ? column[key].render(item[key], item, index)
+                    : item[key]}
+                </td>
+              );
+            })}
+          </tr>
+        </Fragment>
+      );
+    });
+  };
 
-    export default ZyrTable;
-  
+  return (
+    <div className={cn(bem.b())}>
+      <table>
+        <thead>
+          <tr>{getTableHeader()}</tr>
+        </thead>
+        <tbody>{getTableBodyRow()}</tbody>
+      </table>
+    </div>
+  );
+};
+
+export default ZyrTable;
