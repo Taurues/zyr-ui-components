c8fd7a9de2e3b3807164a5ae2279ae51b9a9ee62
zhangyanru <zhangyanru@wshifu.com>
2025-01-08 18:15:37 +0800

    feat: fixed table 列

--- src/packages/ZyrTable/index.stories.tsx
+++ src/packages/ZyrTable/index.stories.tsx
@@ -19,6 +19,7 @@ export const DefaultTable: Story = {
     column: {
       name1: {
         key: "name1",
+        fixed: "left",
         title: (
           <div>
             第1列
@@ -37,11 +38,11 @@ export const DefaultTable: Story = {
       name4: {
         key: "name4",
         title: "第4列",
-        render: (val, item, index) => {
+        render: (val, record, index) => {
           return (
             <div style={{ color: "red" }}>
               <div>render字段值：{val}</div>
-              <div>name3的值：{item.name3}</div>
+              <div>name3的值：{record?.name3}</div>
               <div> 索引：{index}</div>
             </div>
           );
@@ -54,6 +55,76 @@ export const DefaultTable: Story = {
     dataSource: [
       { name1: "1", name2: "2", name3: "3", name4: "4" },
       { name1: "11", name2: "22", name3: "33", name4: "44" },
+      { name1: "111", name2: "222", name3: "333", name4: "444" },
     ],
+    pagination: false,
+  },
+};
+
+/**
+ * 固定列
+ */
+export const FixedTable: Story = {
+  args: {
+    column: {
+      name1: {
+        key: "name1",
+        title: "第1列",
+      },
+      name2: {
+        key: "name2",
+        title: "第2列",
+      },
+      name3: {
+        key: "name3",
+        title: "第3列",
+      },
+      name4: {
+        key: "name4",
+        title: "第4列",
+      },
+      name5: {
+        key: "name5",
+        title: "第5列",
+      },
+      name6: {
+        key: "name6",
+        title: "第6列",
+      },
+      name7: {
+        key: "name7",
+        title: "第7列",
+      },
+      name8: {
+        key: "name8",
+        title: "第8列",
+        fixed: "right",
+        headerStyle: { background: "pink" },
+        cellStyle: { background: "pink" },
+      },
+    },
+    extraColumn: (val) => {
+      return <div style={{ color: "green" }}>name1字段值：{val.name1}</div>;
+    },
+    dataSource: [
+      {
+        name1: "1",
+        name2: "2",
+        name3: "3",
+        name4: "4",
+        name5: "11",
+        name6: "22",
+        name7: "33",
+        name8: "44",
+      },
+    ],
+    pagination: false,
+  },
+  render: (args) => {
+    return (
+      <div style={{ width: "100%", overflowX: "auto" }}>
+        <ZyrTable {...args} />
+      </div>
+    );
   },
 };
--- src/packages/ZyrTable/index.tsx
+++ src/packages/ZyrTable/index.tsx
@@ -2,26 +2,85 @@ import "./index.less";
 import React, { Fragment } from "react";
 import cn from "classnames";
 import { createNamespace } from "../../utils/createBEM";
+import { Pagination, Spin } from "antd";
+import type { PaginationProps, SpinProps } from "antd";
 
 type ItemProps = {
   key: string;
   title: string | React.ReactNode;
   headerStyle?: React.CSSProperties;
   cellStyle?: React.CSSProperties;
-  render: (val: any, item?: Object, index?: number) => React.ReactNode;
+  fixed?: "left" | "right";
+  render?: (val: any, record?: Object, index?: number) => React.ReactNode;
+};
+
+type RowSelectionProps = {
+  /**
+   * 隐藏全选勾选框
+   */
+  hideSelectAll?: boolean;
+  /**
+   * 选中项发生变化时的回调
+   */
+  onChange?: (selectedRowKeys: any, selectedRows: any) => void;
 };
 export interface ZyrTableProps {
+  /**
+   * 表格列的配置
+   */
   column: { [key: string]: ItemProps };
+  /**
+   * 表格额外列的配置
+   */
   extraColumn: React.ReactNode | ((val: any) => React.ReactNode);
-  dataSource: object[];
+  /**
+   * table数据源
+   */
+  dataSource: { [key: string | number]: any }[];
+  /**
+   * 表格行 key 的取值，可以是字符串或一个函数
+   */
+  rowKey?: string | ((record: any) => string);
+  /**
+   * 是否需要表格外边框
+   */
+  bordered?: boolean;
+  /**
+   *页面是否加载中, 配置项参考antd Spin组件
+   */
+  loading?: boolean | SpinProps;
+  /**
+   * 分页器，配置项参考 antd Pagination组件
+   */
+  pagination?: PaginationProps | boolean;
+  /**
+   * 表格行是否可选择
+   */
+  rowSelection?: RowSelectionProps;
 }
 
+const initPagination = {
+  pageSize: 10,
+  defaultCurrent: 1,
+};
+
 const bem = createNamespace("table");
-const ZyrTable = ({ column, extraColumn, dataSource }: ZyrTableProps) => {
+const ZyrTable = ({
+  column,
+  extraColumn,
+  dataSource,
+  rowKey,
+  bordered = false,
+  loading = false,
+  pagination = initPagination,
+}: ZyrTableProps) => {
   const getTableHeader = () => {
     return Object.keys(column).map((key) => {
+      const fixStyle: React.CSSProperties = column[key].fixed
+        ? { position: "sticky", [column[key].fixed]: 0 }
+        : {};
       return (
-        <th key={key} style={{ ...column[key].headerStyle }}>
+        <th key={key} style={{ ...column[key].headerStyle, ...fixStyle }}>
           {column[key].title}
         </th>
       );
@@ -31,7 +90,15 @@ const ZyrTable = ({ column, extraColumn, dataSource }: ZyrTableProps) => {
   const getTableBodyRow = () => {
     return dataSource.map((item, index) => {
       return (
-        <Fragment key={index}>
+        <Fragment
+          key={
+            rowKey
+              ? typeof rowKey === "function"
+                ? rowKey(item)
+                : item[rowKey]
+              : index
+          }
+        >
           <tr className={cn(bem.e("empty"))}></tr>
           <tr className={cn(bem.e("extra"))}>
             <td colSpan={Object.keys(column).length}>
@@ -42,8 +109,11 @@ const ZyrTable = ({ column, extraColumn, dataSource }: ZyrTableProps) => {
           </tr>
           <tr>
             {Object.keys(column).map((key) => {
+              const fixStyle: React.CSSProperties = column[key].fixed
+                ? { position: "sticky", [column[key].fixed]: 0 }
+                : {};
               return (
-                <td key={key} style={{ ...column[key].cellStyle }}>
+                <td key={key} style={{ ...column[key].cellStyle, ...fixStyle }}>
                   {column[key].render
                     ? column[key].render(item[key], item, index)
                     : item[key]}
@@ -58,12 +128,28 @@ const ZyrTable = ({ column, extraColumn, dataSource }: ZyrTableProps) => {
 
   return (
     <div className={cn(bem.b())}>
-      <table>
-        <thead>
-          <tr>{getTableHeader()}</tr>
-        </thead>
-        <tbody>{getTableBodyRow()}</tbody>
-      </table>
+      <Spin
+        {...(typeof loading === "boolean" ? { spinning: loading } : loading)}
+      >
+        <>
+          <table style={{ border: bordered ? "1px solid #f5f5f5" : "none" }}>
+            <thead>
+              <tr>{getTableHeader()}</tr>
+            </thead>
+            <tbody>{getTableBodyRow()}</tbody>
+          </table>
+          {pagination && (
+            <div className={cn(bem.e("pagination"))}>
+              <Pagination
+                size="small"
+                {...(typeof pagination === "object"
+                  ? pagination
+                  : initPagination)}
+              />
+            </div>
+          )}
+        </>
+      </Spin>
     </div>
   );
 };
